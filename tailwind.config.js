7611e00f57b6a54de26b52d5470753169a554e33
Souvik <souvik13109@gmail.com>
2025-01-25 14:01:41 +0530

    home page

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // existing config
+  theme: {
+    extend: {
+      gradients: {
+        'custom-linear-gradient': 'linear-gradient(to bottom, #FFFEFC 0%, #FEFAF5 100%)',
+      },
+    },
+  },
+}
